484aa04ef3af9b04dd0dca433c6e7be518d93af9
Surry Mowery <surrykj@gmail.com>
2022-05-11 09:20:42 -0700

    navigation component setup

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,48 +10,22 @@ const navigationItems = [
   { url: '#contact', label: 'Contact' }
 ]
 
-
-// const mainNavItems = [
-//   { url: '/blog', icon: blog, label: 'Articles' },
-//   { url: '/projects', icon: projects, label: 'Projects' },
-//   { url: '/me', icon: floppy, label: 'About me' },
-// ]
-// {mainNavItems.map((item) => (
-//   <Link to={item.url} key={item.label} activeClassName="active">
-//     <img src={item.icon} alt={item.label} />
-//     <div className="tooltip">{item.label}</div>
-//   </Link>
-// ))}
-
-
 function Navigation() {
 
   return (
-    <nav id="navbar" className="navbar">
-      <Logo />
-      {navigationItems.map((navItem) => (
-        <HashLink to={navItem.url} key={navItem.label} className="gradient-link nav-links">{navItem.label}</HashLink>
-      ))}
+    <div className="Navigation">
+      <nav id="navbar" className="navbar">
+        <Logo />
+        <div>
+          {navigationItems.map((navItem) => (
+            <HashLink to={navItem.url} key={navItem.label} className="gradient-link nav-links">{navItem.label}</HashLink>
+          ))}
+        </div>
+      </nav>
       <a id="hamburger-icon" href="javascript:void(0);" >
         <FontAwesomeIcon icon={faBars} aria-hidden="true" className="hamburger-icon" />
       </a>
-
-      {/* <p><a id="logo" class="logo" href="#top">Surry Jones Mowery</a></p> */}
-      {/* <ul>
-        <li>
-          <a id="hamburger-icon" href="javascript:void(0);" class="hamburger-icon"><i class="fa fa-bars" aria-hidden="true"></i></a>
-        </li>
-        <li>
-          <a class="gradient-link nav-links" href="#about">About</a>
-        </li>
-        <li>
-          <a class="gradient-link nav-links" href="#projects">Projects</a>
-        </li>
-        <li>
-          <a class="gradient-link nav-links" href="#contact">Contact</a>
-        </li>
-      </ul> */}
-    </nav>
+    </div>
   )
 }
 
