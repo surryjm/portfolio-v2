8c54815af41be0f327833c711772ef4910d31932
Surry Mowery <surrykj@gmail.com>
2022-05-15 10:42:40 -0700

    starting to add other sections

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ function App() {
   return (
     <div className="App">
       <header id="#top" className="header-section">
+      {/*-- GRADIENT HEADER --*/}
         <div className="gradient-header"></div>
         <div className="wrapper">
+        {/*-- NAV --*/}
           <Navigation />
         </div>
       </header>
+      <div className="angle-nav"></div>
+      <main id="main">
+
+      </main>
     </div>
   );
 }
