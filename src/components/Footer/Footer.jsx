a289ebefacf1e0519b89647eb815fb48485a6c78
Surry Mowery <surrykj@gmail.com>
2022-07-03 12:22:08 -0700

    adjusted links that open in new tab in footer, project components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,16 +12,16 @@ export default function Footer() {
       <p className="subtitle">Get in Touch</p>
       <p>Feel free to connect. I'd love to hear from you!</p>
       <ul className="footer-line">
-        <li><a className="gradient-link" href="mailto:surryjm@gmail.com" target="_blank">
+        <li><a className="gradient-link" href="mailto:surryjm@gmail.com" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" className="footer-fontAwesome" />Email</a>
           </li>
-        <li><a className="gradient-link" href="https://www.linkedin.com/in/surryjmowery/" target="_blank">
+        <li><a className="gradient-link" href="https://www.linkedin.com/in/surryjmowery/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faLinkedin} aria-hidden="true" className="footer-fontAwesome" />LinkedIn</a>
         </li>
-        <li><a className="gradient-link" href="https://github.com/surryjm" target="_blank">
+        <li><a className="gradient-link" href="https://github.com/surryjm" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faGithub} aria-hidden="true" className="footer-fontAwesome" />Github</a>
         </li>
-        <li><a className="gradient-link" href="https://codepen.io/surry/pens/showcase" target="_blank">
+        <li><a className="gradient-link" href="https://codepen.io/surry/pens/showcase" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faCodepen} aria-hidden="true" className="footer-fontAwesome" />CodePen</a>
         </li>
       </ul>
