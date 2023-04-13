0955c82b48c8f767f0a302ac49fd49cf9446555b
Surry Mowery <surrykj@gmail.com>
2023-04-12 21:04:15 -0700

    removed extra images from projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -6,24 +6,15 @@ export default function Project( {projects} ) {
     projects.map((item, index) => (
     <div className="project-tile" key={index}>
       <div className="project-image-text">
-        <picture>
-          <source 
-            sizes="(min-width: 501px) 452px,
-                   (min-width: 769px) 337px,
-                   (min-width: 1025px) 452px,
-                   (min-width: 1201px) 552px"  
-            srcSet={`${item.webp337} 337w, ${item.webp452} 452w, ${item.webp552} 552w`}
-            type="image/webp" />
-          <source 
-            srcSet={item.jpg}
-            type="image/jpg" />
+        <div className="container-img">
           <img 
-            src={item.jpg}
+            src={item.photo}
             alt={item.title} 
             width="552"
             height="345" 
-            className="project-image"/>
-        </picture>
+            className="project-image"
+          />
+        </div>
         <div className="project-title">
           <div>
             <h3>{item.title}</h3>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,29 +2,12 @@ import React from 'react';
 import Project from '../Project/Project';
 import './Projects.scss';
 import coffeeJpg from '../../assets/coffee-shop-image.jpg';
-import coffee337 from '../../assets/coffee-shop-image-337.webp';
-import coffee452 from '../../assets/coffee-shop-image-452.webp';
-import coffee552 from '../../assets/coffee-shop-image-552.webp';
 import educationJpg from '../../assets/education-app-image.jpg';
-import education337 from '../../assets/education-app-image-337.webp';
-import education452 from '../../assets/education-app-image-452.webp';
-import education552 from '../../assets/education-app-image-552.webp';
 import oliverWymanConnectPng from '../../assets/oliver-wyman-connect-homepage-image.png';
-import oliverWymanConnect337 from '../../assets/oliver-wyman-connect-homepage-image-337.png';
-import oliverWymanConnect452 from '../../assets/oliver-wyman-connect-homepage-image-452.png';
-import oliverWymanConnect552 from '../../assets/oliver-wyman-connect-homepage-image-552.png';
 import wordleishPng from '../../assets/wordle-ish-image.png';
-import wordleish337 from '../../assets/wordle-ish-image-337.png';
-import wordleish452 from '../../assets/wordle-ish-image-452.png';
-import wordleish552 from '../../assets/wordle-ish-image-552.png';
 import rockPaperScissorsPng from '../../assets/rock-paper-scissors-image.png';
-import rockPaperScissors337 from '../../assets/rock-paper-scissors-image-337.png';
-import rockPaperScissors452 from '../../assets/rock-paper-scissors-image-452.png';
-import rockPaperScissors552 from '../../assets/rock-paper-scissors-image-552.png';
 import seattleLiteracyPng from '../../assets/seattle-literacy-image.png';
-import seattleLiteracy337 from '../../assets/seattle-literacy-image-337.png';
-import seattleLiteracy452 from '../../assets/seattle-literacy-image-452.png';
-import seattleLiteracy552 from '../../assets/seattle-literacy-image-552.png';
+import calculatorPng from '../../assets/calculator-image.png';
 
 
 export default function Projects() {
@@ -33,10 +16,7 @@ export default function Projects() {
       title: "Alumni Network homepage", 
       description: "Built Wordpress page from client mockups using HTML, CSS, JavaScript and PHP, and configured and styled cookie banner with OneTrust.", 
       siteURL: "https://www.oliverwymanconnect.com/login/", 
-      jpg: `${oliverWymanConnectPng}`,
-      webp337: `${oliverWymanConnect337}`,
-      webp452: `${oliverWymanConnect452}`,
-      webp552: `${oliverWymanConnect552}`
+      photo: `${oliverWymanConnectPng}`
     },
     
     {
@@ -44,10 +24,7 @@ export default function Projects() {
       description: "Built prototype site for coffee shop using Vue, Nuxt, Axios and Sass and deployed with Netlify.", 
       siteURL: "https://festive-joliot-8258ef.netlify.app/", 
       repoURL: "https://github.com/surryjm/coffee-shop",
-      jpg: `${coffeeJpg}`,
-      webp337: `${coffee337}`,
-      webp452: `${coffee452}`,
-      webp552: `${coffee552}`
+      photo: `${coffeeJpg}`
     },
 
     {
@@ -56,30 +33,30 @@ export default function Projects() {
       attribute: "Background image by Grey Wight on Unsplash.", 
       siteURL: "https://surryjm.github.io/wordle-clone/", 
       repoURL: "https://github.com/surryjm/wordle-clone",
-      jpg: `${wordleishPng}`,
-      webp337: `${wordleish337}`,
-      webp452: `${wordleish452}`,
-      webp552: `${wordleish552}`
+      photo: `${wordleishPng}`
     },
 
     {
       title: "Seattle Literacy site enhancements", 
       description: "Volunteer project for their Wordpress site. Set up staging environment and implemented CSS, accessibility and SEO improvements.", 
       siteURL: "https://seattleliteracy.org/", 
-      jpg: `${seattleLiteracyPng}`,
-      webp337: `${seattleLiteracy337}`,
-      webp452: `${seattleLiteracy452}`,
-      webp552: `${seattleLiteracy552}`
+      photo: `${seattleLiteracyPng}`
+    },
+
+    {
+      title: "Calculator", 
+      description: "Simple calculator app, made with HTML, CSS and JavaScript, and deployed with GitHub Pages.",
+      attribute: "Background image by Grey Wight on Unsplash.", 
+      siteURL: "https://surryjm.github.io/calculator/", 
+      repoURL: "https://github.com/surryjm/calculator",
+      photo: `${calculatorPng}`
     },
   
     {
       title: "Education App", 
       description: "Group project app that allows teachers, students and parents to log in and view the site according to their role. I was responsible for connecting the front-end to the back-end. Built with MongoDB, Express, Node.js, Mustache and Bootstrap, deployed with Heroku.", 
       repoURL: "https://github.com/JS330-Education-App/final-project",
-      jpg: `${educationJpg}`,
-      webp337: `${education337}`,
-      webp452: `${education452}`,
-      webp552: `${education552}`
+      photo: `${educationJpg}`
     },
   
     {
@@ -88,10 +65,7 @@ export default function Projects() {
       attribute: "Background image by zlatko_plamenov on Freepik.", 
       siteURL: "https://hardcore-kilby-aa9ec0.netlify.app/", 
       repoURL: "https://bitbucket.org/surryjm/jscript-310b-final-project-winter-2021/src/master/",
-      jpg: `${rockPaperScissorsPng}`,
-      webp337: `${rockPaperScissors337}`,
-      webp452: `${rockPaperScissors452}`,
-      webp552: `${rockPaperScissors552}`
+      photo: `${rockPaperScissorsPng}`,
     }
   ]
 
